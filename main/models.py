50c8cd44ee58b8647e6bd31644d156ec87d3c11e
Fec16 <jasonkentwinata@gmail.com>
2023-09-13 10:55:27 +0700

    Added unit tests

--- main/models.py
+++ main/models.py
@@ -1,6 +1,10 @@
 from django.db import models
+import uuid
 
 class Item(models.Model):
     name = models.CharField(max_length=255)
     amount = models.IntegerField()
+    type = models.TextField(default= "default_quality")
+    quality = models.CharField(max_length=7, default= "default_quality")
     description = models.TextField()
+    id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from .models import Item
 
-# Create your tests here.
+class ItemTestCase(TestCase):
+    
+    def setUp(self):
+        self.item = Item.objects.create(
+            name = "Mora",
+            amount = 1000,
+            type = "Common Currencies",
+            quality = "3 stars",
+            description = "Common currency. The one language everybody speaks.",
+        )
+
+    def test_item(self):
+        item_from_databases = Item.objects.get(id = self.item.id)
+
+        self.assertEqual(item_from_databases.name, "Mora")
+        self.assertEqual(item_from_databases.amount, 1000)
+        self.assertEqual(item_from_databases.type, "Common Currencies")
+        self.assertEqual(item_from_databases.quality, "3 stars")
+        self.assertEqual(item_from_databases.description, "Common currency. The one language everybody speaks.")
