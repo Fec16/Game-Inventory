5259ad9e458a0bf9be020665b008327321ef1c82
Fec16 <jasonkentwinata@gmail.com>
2023-09-16 18:26:15 +0700

    Commit Tugas3

--- main/models.py
+++ main/models.py
@@ -4,7 +4,7 @@ import uuid
 class Item(models.Model):
     name = models.CharField(max_length=255)
     amount = models.IntegerField()
-    type = models.TextField(default= "default_quality")
+    type = models.TextField(default= "default_type")
     quality = models.CharField(max_length=7, default= "default_quality")
     description = models.TextField()
     id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
